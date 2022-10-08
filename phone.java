d7fb5b9462fadaab658d3410735bdc025339af28
Rampratahp <114553997+Rampratahp@users.noreply.github.com>
2022-10-08 18:59:41 +0530

    Add files via upload

---
+++ phone.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package A;
+
+/**
+ * @author Muni
+ *
+ */
+public  abstract class phone {
+
+	abstract String phone_name();
+	abstract String phone_color();
+	abstract int phone_cost();
+	abstract int phone_emi();
+	abstract int phone_ram();
+	abstract int phone_storege();
+	abstract int phone_lonchyear();
+	abstract int phone_gb();
+	abstract void phonedetails();
+	int phone_storage() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+}
