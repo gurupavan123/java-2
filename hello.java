4652b535ea141a3596a80b10c2644fc15e6fa48f
gurupavan123 <114553890+gurupavan123@users.noreply.github.com>
2022-10-08 18:52:35 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,24 @@
+package pavan;
+
+import java.util.Scanner ;
+
+
+public class hello {
+ 
+
+
+	public static void main(String[] args) {
+	
+
+
+		System.out.println("enter any Number");
+	Scanner a = new Scanner(System.in);
+			int day=a.nextInt(),s=day*day;
+			
+			System.out.println("enter any Number"+"    "+ s);
+			
+			
+			
+			}
+	}
+
