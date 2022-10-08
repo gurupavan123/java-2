6010351c9acd16b23e49f8db816c90b58809c5ff
gurupavan123 <114553890+gurupavan123@users.noreply.github.com>
2022-10-08 18:00:55 +0530

    Add files via upload

---
+++ Even_Numbers.java
@@ -0,0 +1,20 @@
+package pavan;
+
+public class Even_Numbers {
+
+	public static void main(String[] args) {
+		
+		
+		for(int i=1;i<=50;i++) {
+			
+			if(i%2==0) {
+				System.out.println(i+ "");
+				
+				
+			}
+		}
+		// TODO Auto-generated method stub
+   
+	}
+
+}
